c45da383132f42bdc8a9d26a13fb865aed4f2748
Dan Nascimbeni <dnascimb@gmail.com>
2015-11-23 21:16:50 -0800

    changes for jeff

--- calculator.js
+++ calculator.js
@@ -21,7 +21,7 @@ var constants = {
    OIL_LOSS: .2,
    SUBA_LOSS_AFTER_CURING: 2,
    SUBA_LOSS_AFTER_BUTTER_INFUSION: .30,
-   SUBA_LOSS_AFTER_OIL_INFUSION: .2
+   SUBA_LOSS_AFTER_OIL_INFUSION: .1
 };
 Calculator.prototype.getConstantButterLoss = function(){return constants.BUTTER_LOSS;};
 Calculator.prototype.getConstantOilLoss = function(){return constants.OIL_LOSS;};
@@ -44,6 +44,14 @@ function Calculator() {
 	this.subACalculatedMassPerGramForInfusion = 0;
 	this.multiplier = 0;
 	this.subAPerSpecificSubB = 0;
+	this.step2Result = 0;
+	this.step3Result = 0;
+	this.step4Result = 0;
+	this.step5Result = 0;
+	this.step6Result = 0;
+	this.step7Result = 0;
+	this.step8Result = 0;
+	this.step9Result = 0;
 	this.shouldRound = false;
 }
 
@@ -54,12 +62,97 @@ function Calculator() {
 Calculator.prototype.getCuredSubAPercent = function() {
 	if(!this.subA_startingPercentage)
 		return 0;
-	this.curedSubAPercent = this.subA_startingPercentage - constants.SUBA_LOSS_AFTER_CURING;
+
+	if(this.substanceBType == constants.INFUSION_TYPE_BUTTER)
+		this.curedSubAPercent = this.subA_startingPercentage * .7;
+	else
+		this.curedSubAPercent = this.subA_startingPercentage * .7;
+
 	if(this.shouldRound)
 		return this.curedSubAPercent.toFixed(2);
 	else
 		return this.curedSubAPercent;
 };
+
+//step 2
+Calculator.prototype.getStep2 = function() {
+
+ 	this.step2Result = this.getCuredSubAPercent();
+
+	return this.step2Result;
+};
+//step 3
+Calculator.prototype.getStep3 = function() {
+
+	this.step3Result = this.getStep2() * 10;
+
+	return this.step3Result;
+
+};
+//step 4
+Calculator.prototype.getStep4 = function() {
+	if(!this.subA_finalMass)
+		return 0;
+
+	this.step4Result = this.getStep3() * this.subA_finalMass;
+
+	return this.step4Result;
+};
+//step 5
+Calculator.prototype.getStep5 = function() {
+	if(!this.subB_startingMassGrams)
+		return 0;
+	
+	var a = this.getStep4();
+	this.step5Result = a / this.subB_startingMassGrams;
+
+	return this.step5Result;
+};
+
+//step 6
+Calculator.prototype.getStep6 = function() {
+	var a = this.getStep5();
+	if(this.substanceBType == constants.INFUSION_TYPE_BUTTER)
+		var b = this.getSubBMassAfterInfusion() * constants.BUTTER_GRAMS_IN_TABLESPOON;
+	else
+		var b = this.getSubBMassAfterInfusion() * constants.OIL_GRAMS_IN_TABLESPOON;
+
+	if(this.substanceBType == constants.INFUSION_TYPE_BUTTER)
+		var c = (a * b) * (1 - constants.SUBA_LOSS_AFTER_BUTTER_INFUSION); 
+	else
+		var c = (a * b) * (1 - constants.SUBA_LOSS_AFTER_OIL_INFUSION);
+
+	this.step6Result = c;
+
+	return this.step6Result;
+};
+
+//step 7
+Calculator.prototype.getStep7 = function(value) {
+	if(!value)
+		return 0;
+
+	var a = this.getStep5();
+
+	if(this.substanceBType == constants.INFUSION_TYPE_BUTTER)
+		var b = value * constants.BUTTER_GRAMS_IN_TABLESPOON;
+	else
+		var b = value * constants.OIL_GRAMS_IN_TABLESPOON;
+
+	var c = a * b;
+
+	if(this.substanceBType == constants.INFUSION_TYPE_BUTTER)
+		this.step7Result = c * (1-constants.SUBA_LOSS_AFTER_BUTTER_INFUSION);
+	else
+		this.step7Result = c * (1-constants.SUBA_LOSS_AFTER_OIL_INFUSION);
+
+	return this.step7Result;
+};
+
+
+
+
+
 
 //
 // This function returns the percentage amount of SubstanceA after infusion with SubstanceB
@@ -67,7 +160,7 @@ Calculator.prototype.getCuredSubAPercent = function() {
 Calculator.prototype.getSubAPercentAfterInfusion = function() {
 	if(!this.subA_startingPercentage || !this.substanceBLoss || !this.substanceBType)
 		return 0;
-	//console.log("cured sub loss: " + this.substanceBLoss);
+
 	if(this.substanceBType == constants.INFUSION_TYPE_BUTTER)
 		this.substanceBLoss = constants.SUBA_LOSS_AFTER_BUTTER_INFUSION;//butter
 	else
@@ -118,114 +211,6 @@ Calculator.prototype.getCuredSubACalculatedMassTotal = function() {
 
 
 
-
-
-
-
-//
-// This function returns the multiplier to use when calculating recipe amounts of THC MG per serving
-//
-Calculator.prototype.getMultiplier = function() {
-	if(!this.subA_finalMass || !this.substanceBType)
-		return 0;
-	var total_thcmg = this.getSubACalculatedMassPerGramForInfusion() * this.subA_finalMass;
-
-	var a = this.subBAmountInRecipe; //56.x
-	//console.log("b: " + b);
-
-	var b = this.subA_finalMass; //5g
-
-	var numerator = a * b;
-	var denominator = total_thcmg;
-
-console.log("a: " + a);
-console.log("b: " + b);
-console.log("numerator: " + numerator);
-console.log("denominator: " + denominator);
-
-
-
-	this.multiplier = numerator / denominator;
-
-	console.log("multiplier: " + this.multiplier);
-	if(this.shouldRound)
-		return this.multiplier.toFixed(2);
-	else
-		return this.multiplier;
-};
-
-//
-// This function returns the SubstanceA (mg) mass amount per SubstanceB (g) mass amount
-//
-Calculator.prototype.getSubAPerSpecificGramsSubB = function(value) {
-	if(!value)
-		return 0;
-
-	console.log("value: " + value);
-	
-	var a = this.getMultiplier();
-	var b = value;
-
-	this.subAPerSpecificSubB = a * b;
-	if(this.shouldRound)
-		return this.subAPerSpecificSubB.toFixed(2);
-	else
-		return this.subAPerSpecificSubB;
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-// This function returns the SubstanceA (mg) mass amount per SubstanceB (g) mass amount
-//
-Calculator.prototype.getSubAMassPerSingleSubBMassUnit = function() {
-	if(this.substanceBType)
-	{
-		var a = this.getSubACalculatedMassPerGramForInfusion();
-
-	if(this.substanceBType == constants.INFUSION_TYPE_BUTTER)
-		var b = this.getSubBMassAfterInfusion() * constants.BUTTER_GRAMS_IN_TABLESPOON;
-	else
-		var b = this.getSubBMassAfterInfusion() * constants.OIL_GRAMS_IN_TABLESPOON;
-	
-//	console.log("a: " + a);
-//	console.log("b: " + b);
-
-	this.curedSubAMassPerSingleSubBMassUnit = a / b;
-	if(this.shouldRound)
-		return this.curedSubAMassPerSingleSubBMassUnit.toFixed(2);
-	else
-		return this.curedSubAMassPerSingleSubBMassUnit;
-	}
-	else
-		return 0;
-};
-
-
-//
-// This function returns the SubstanceA mass amount per the passed in SubstanceB mass amount 
-//
-Calculator.prototype.getSubAMassPerSpecificSubBMass = function(value) {
-	if(!value)
-		return 0;
-	this.curedSubAMassPerCertainAmountofSubBMass = this.getSubAMassPerSingleSubBMassUnit() * value;
-	if(this.shouldRound)
-		return this.curedSubAMassPerCertainAmountofSubBMass.toFixed(2);
-	else
-		return this.curedSubAMassPerCertainAmountofSubBMass;
-};
-
 //
 // This function returns the SubstanceB mass amount after infusion 
 //
@@ -250,25 +235,6 @@ Calculator.prototype.getSubBMassAfterInfusion = function() {
 		return this.subB_massAfterInfusion.toFixed(2);
 	else
 		return this.subB_massAfterInfusion;
-};
-
-//
-// This function returns the SubstanceA mass amount per the passed in SubstanceB mass amount 
-//
-Calculator.prototype.getSubAMassPerNumberOfServings = function(subB_massInTablespoons, numberOfServings) {
-	if(!subB_massInTablespoons || !numberOfServings)
-		return 0;
-
-	if(this.substanceBType == constants.INFUSION_TYPE_BUTTER)
-		this.subA_massPerServing = this.getSubAMassPerSpecificSubBMass(subB_massInTablespoons * constants.BUTTER_GRAMS_IN_TABLESPOON) / numberOfServings;
-	else
-		this.subA_massPerServing = this.getSubAMassPerSpecificSubBMass(subB_massInTablespoons * constants.OIL_GRAMS_IN_TABLESPOON) / numberOfServings;
-		
-
-	if(this.shouldRound)
-		return this.subA_massPerServing.toFixed(2);
-	else
-		return this.subA_massPerServing;
 };
 
 
@@ -302,23 +268,18 @@ Calculator.prototype.setFinalSubAMass = function(value) {
 Calculator.prototype.setMassResultSubB = function(value) {
 	if(value)
 	{
-//		console.log("value: " + value);
-//		console.log("constants.BUTTER_GRAMS_IN_TABLESPOON: " + constants.BUTTER_GRAMS_IN_TABLESPOON);
 		if(this.substanceBType == constants.INFUSION_TYPE_BUTTER) {
-//			console.log("mult: " + (value * constants.BUTTER_GRAMS_IN_TABLESPOON));
 			this.subB_startingMassGrams = value * constants.BUTTER_GRAMS_IN_TABLESPOON; //entered in tablespoons and we convert to grams
 		} else {
 			this.subB_startingMassGrams = value * constants.OIL_GRAMS_IN_TABLESPOON;
 		}
 	}
-//	console.log("subB_startingMassGrams orig: " + this.subB_startingMassGrams);
 };
 
 //
 // This function sets the MassResultSubB property
 // 
 Calculator.prototype.setSubBType = function(value) {
-	//console.log("value: " + value);
 	if(value)
 		this.substanceBType = value; //entered in tablespoons and we convert to grams
 };
